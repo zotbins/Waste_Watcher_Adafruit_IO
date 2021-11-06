07cb8eb02641fa2daaf5ecd87be62efe292fe044
Owen Yang <33404602+okyang@users.noreply.github.com>
2021-11-05 22:08:28 -0700

    HCSR-04 base code (#4)
    * include HCSR04 base code
    
    * update readme
    
    * add header guards

---
+++ waste_watcher_adafruitIO/src/template-config.h
@@ -0,0 +1,20 @@
+// Use this as a template to create your own "config.h" file. 
+// "config.h" will remain  untracked by the ".gitignore" file to avoid
+// accidentally tracking any sensitive information.
+
+#ifndef CONFIG
+#define CONFIG
+// ultrasonic sensor pins
+const int ultrasonicTrigPin = 13;
+const int ultrasonicEchoPin = 12;
+
+// deep sleep
+#define uS_TO_S_FACTOR 1000000 //conversion factor for usec to sec
+#define TIME_TO_SLEEP 1800   // TIME ESP32 will sleep for 1800 sec
+
+// system variables
+bool debug = true; // set false, if not debugging
+const int binHeight = 50; // units: cm, used to calculate fullness
+const int retries = 5; // # of retries for collecting fullness
+
+#endif
