922bae87ba72dc9a3fd21c3e4640013553ada2f6
Owen Yang <33404602+okyang@users.noreply.github.com>
2021-11-27 16:16:59 -0800

    Capture Image and Encode to Base64 string (#9)
    * capture image and encode to base64 string
    
    * refactoring Image related code

--- waste_watcher_adafruitIO/src/main.cpp
+++ waste_watcher_adafruitIO/src/main.cpp
@@ -1,19 +1,23 @@
 #include <Arduino.h>
 
-#include "esp_camera.h"
 #include "soc/soc.h"           // Disable brownout problems
 #include "soc/rtc_cntl_reg.h"  // Disable brownout problems
-#include "driver/rtc_io.h"
 
 #include <WiFi.h>
 
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
+//#include <base64.h>
 #include "Fullness.h"
+#include "Image.h"
 
 // create a `config.h` file based off of `template-config.h`
 #include "config.h"
+
+// Adafruit IO 
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+bool debug = true;
 
 void setup() {
   if (debug) Serial.begin(115200);
@@ -34,19 +38,25 @@ void setup() {
     Serial.println(io.statusText());
   }
   
-  // ---- ultrasonic -----
+  // ---- set pins -----
   pinMode(ultrasonicTrigPin, OUTPUT);
   pinMode(ultrasonicEchoPin, INPUT);
+  pinMode(flashLED, OUTPUT);
 
   // ---- telemetry variables ----
   int fullness;
+  String base64Image; //base-64 encoded string to represent a JPG image
 
   // ---- Read Fullness ----
   if (debug) Serial.println("\nReading fullness...");
   fullness = calculateFullness(ultrasonicTrigPin, ultrasonicEchoPin, binHeight, 0, binHeight, retries);
   if (debug) Serial.println(fullness);
   
-  // TODO: Get Picture
+  // ---- Get Picture ----
+  if (debug) Serial.println("\nCapturing image...");
+  configInitCamera(debug);
+  base64Image = takePhoto(debug);
+  if (debug) Serial.println("Image captured.");
 
   // ----- Send Telemetry -----
   io.run();
